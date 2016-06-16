2213bd80a99e6ad0a22bfa3d2b19546edf92b039
Dave Richardson <richardsongd@ornl.gov>
2016-06-16 18:56:31 -0400

    Maybe got it worked out

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+export { default as Axis } from './Axis'
+export { default as Bar } from './Bar'
 export { default as Chart } from './Chart'
 export { default as Choropleth } from './Choropleth'
 export { default as Heatmap } from './Heatmap'
 export { default as Histogram } from './Histogram'
+export { default as Legend } from './Legend'
 export { default as Scatterplot } from './Scatterplot'
