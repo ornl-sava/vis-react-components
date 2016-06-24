540142c0a01a0fb64daa2d97171486333bd3ee39
Dave Richardson <richardsongd@ornl.gov>
2016-06-24 11:30:41 -0400

    need all components for debuging in alert-vis-ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 export { default as Chart } from './Chart'
-// export { default as Axis } from './Axis'
-// export { default as Legend } from './Legend'
-// export { default as Settings } from './Settings'
-
+export { default as Axis } from './Axis'
+export { default as Legend } from './Legend'
+export { default as Settings } from './Settings'
 export { default as Choropleth } from './Choropleth'
 export { default as Heatmap } from './Heatmap'
 export { default as Histogram } from './Histogram'
