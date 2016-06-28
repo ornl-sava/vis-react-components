ff5cebe01d2bc571699b7dfde231faf79f3ca04a
Pachuilo <ap2@z8cimac.ornl.gov>
2016-06-28 09:47:03 -0400

    removed icono

--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import routes from './routes'
 import './vis.css'
-import './icono.min.css'
 
 console.info('Serving examples from vis/examples')
 ReactDom.render(
