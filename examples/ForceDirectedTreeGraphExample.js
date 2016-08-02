dc26d6b22433caeaacf2ef2f5e1ad195a7a1b2b0
Pena <b7x@ba.ornl.gov>
2016-08-02 13:50:18 -0400

    forceDirectedGraph: circumshaker data added

--- examples/ForceDirectedTreeGraphExample.js
+++ examples/ForceDirectedTreeGraphExample.js
@@ -10,11 +10,33 @@ import storyData from '../examples/data/for-hci/stories.json'
 
 // DISPLAYS INFO OF SVG OBJECT THAT MOUSE IS ON
 // USED IN THE COMPONENT ALONGSIDE THE TOOLTIP MODULE
-const toolTipFunction = (tooltipData) => {
-  let d = tooltipData
+// const toolTipFunction = (data) => {
+//   let name = 'Node '
+//   if (data.parent == null) {
+//     name = 'Root '
+//   } else if (data.children != null) {
+//     name = 'Parent '
+//   }
+//   name = name + data.key
+//   var toolTip =
+//     '<span class="title">' + name + '</span>' +
+//     '</span>Number of Topics: ' + d3.format(',')(data.data.events.length) +
+//     '<br /><small>'
+//   return toolTip
+// }
+const toolTipFunctionIP = (data) => {
+  let name = 'Node '
+  if (data.parent == null) {
+    name = 'Root '
+  } else if (data.children != null) {
+    name = 'Parent '
+  }
+  name = name + data.key
+  let key = data.data.key_as_string
+  if (key == null) { key = '' }
   var toolTip =
-    '<span class="title">' + d.label + '</span>' +
-    '</span>Number of Topics: ' + d3.format(',')(d.counts) +
+    '<span class="title">' + name + '</span>' +
+    '</span>Key: ' + (key) +
     '<br /><small>'
   return toolTip
 }
@@ -177,17 +199,149 @@ let makeTreeData = () => {
   })
   return treeList
 }
+console.log('FDTGE-makeTreeData', makeTreeData())
+
+// 'source_ip'
+// 'dest_ip'
+const data1 = {
+  'source_ip': '124.83.248.123',
+  'dest_ip': [
+    {
+      'key': 2751196930,
+      'key_as_string': '163.251.239.2',
+      'doc_count': 5,
+      'source_ip': [
+        {
+          'key': 1755075020,
+          'key_as_string': '104.156.81.204',
+          'doc_count': 2
+        },
+        {
+          'key': 1758454856,
+          'key_as_string': '104.207.228.72',
+          'doc_count': 2
+        },
+        {
+          'key': 1753404735,
+          'key_as_string': '104.130.213.63',
+          'doc_count': 1
+        },
+        {
+          'key': 1753429508,
+          'key_as_string': '104.131.54.4',
+          'doc_count': 1
+        },
+        {
+          'key': 1755076044,
+          'key_as_string': '104.156.85.204',
+          'doc_count': 1
+        },
+        {
+          'key': 3494830881,
+          'key_as_string': '208.78.227.33',
+          'doc_count': 1
+        }
+      ]
+    },
+    {
+      'key': 2751196932,
+      'key_as_string': '163.251.239.4',
+      'doc_count': 3,
+      'source_ip': [
+        {
+          'key': 1747283813,
+          'key_as_string': '104.37.111.101',
+          'doc_count': 3
+        },
+        {
+          'key': 1755075020,
+          'key_as_string': '104.156.81.204',
+          'doc_count': 1
+        },
+        {
+          'key': 1755076044,
+          'key_as_string': '104.156.85.204',
+          'doc_count': 1
+        },
+        {
+          'key': 1757415067,
+          'key_as_string': '104.192.6.155',
+          'doc_count': 1
+        },
+        {
+          'key': 3107708684,
+          'key_as_string': '185.59.223.12',
+          'doc_count': 1
+        }
+      ]
+    },
+    {
+      'key': 2751196931,
+      'key_as_string': '163.251.239.3',
+      'doc_count': 2,
+      'source_ip': [
+        {
+          'key': 1753361025,
+          'key_as_string': '104.130.42.129',
+          'doc_count': 5
+        },
+        {
+          'key': 1755075020,
+          'key_as_string': '104.156.81.204',
+          'doc_count': 2
+        },
+        {
+          'key': 1755076044,
+          'key_as_string': '104.156.85.204',
+          'doc_count': 1
+        },
+        {
+          'key': 3494830881,
+          'key_as_string': '208.78.227.33',
+          'doc_count': 1
+        }
+      ]
+    },
+    {
+      'key': 2751135764,
+      'key_as_string': '163.251.0.20',
+      'doc_count': 1,
+      'source_ip': []
+    },
+    {
+      'key': 3444397697,
+      'key_as_string': '205.77.86.129',
+      'doc_count': 1,
+      'source_ip': []
+    }
+  ]
+}
+let hier = d3.hierarchy(data1, (d) => {
+  if (d.dest_ip != null) {
+    return d.dest_ip
+  } else if (d.source_ip != null) {
+    return d.source_ip
+  } else {
+    return d.children
+  }
+})
+console.log('FDGTE-hier', hier)
+console.log('FDGTE-hierDescendents', hier.descendants())
+// let apple = {tomate: 0}
+// let acc = 'tomate'
+// console.log('FDGTE-apple', apple[acc])
 
 const treeChartProps = {
-  tipFunction: toolTipFunction,
-  data: makeTreeData()
+  tipFunction: toolTipFunctionIP,
+  data: data1,
+  childAccessors: ['source_ip', 'dest_ip']
 }
 
 class TopicsContainer extends React.Component {
   render () {
     return (
       <div>
-        {<Chart className='col-md-12' tipFunction={toolTipFunction} yAxis={false} xAxis={false} height={1000} >
+        {<Chart className='col-md-12' tipFunction={toolTipFunctionIP} yAxis={false} xAxis={false} height={1000} >
           <ForceDirectedGraphTree {...treeChartProps} />
         </Chart>}
       </div>
--- src/ForceDirectedGraphTree.js
+++ src/ForceDirectedGraphTree.js
@@ -20,7 +20,15 @@ class ForceDirectedGraph extends React.Component {
     this.nodes = []
     this.links = []
     this.data = props.data
-    this.rootNode = d3.hierarchy(this.data)
+    this.rootNode = d3.hierarchy(this.data, (d) => {
+      for (let i = 0; i < props.childAccessors.length; i++) {
+        let data = d[props.childAccessors[i]]
+        if (data != null && typeof data === 'object') {
+          return data
+        }
+      }
+      return d.children
+    })
       .sum((d) => { return d.value })
       .sort((a, b) => { return b.height - a.height || b.value - a.value })
     this.setTree(props)
@@ -46,9 +54,7 @@ class ForceDirectedGraph extends React.Component {
     this.pos = new Array(2)
     this.nodePos = new Array(2)
     this.isDrag = false
-    this.onDrag = this.onDrag.bind(this)
     this.onDragStart = this.onDragStart.bind(this)
-    this.onDragEnd = this.onDragEnd.bind(this)
     this.hidingNodes = []
   }
 
@@ -75,7 +81,7 @@ class ForceDirectedGraph extends React.Component {
       this.simulation.restart()
       this.isDrag = false
     } else {
-      console.log('FDG-onDC-simOff', this.links)
+      console.log('FDG-onDC-simOff')
       this.simOn = true
       this.isDrag = false
       this.setState({nodes: this.nodes, links: this.links})
@@ -86,22 +92,14 @@ class ForceDirectedGraph extends React.Component {
 
   onEnter (event) {
     let target = this.getDatum(event.target)
-    // console.log('FDGT-onE', target)
-    let type = 'Node '
-    if (target.data.events.indexOf('parent-') >= 0) {
-      type = 'Parent '
-    } else if (target.data.events.indexOf('root-') >= 0) {
-      type = 'Root '
-    }
-    let tooltipD = {label: type + event.target.getAttribute('data-key') + ' at Hour ' + target.data.hour, counts: target.data.events.length}
     if (target && this.props.tipFunction) {
-      this.tip.show(event, tooltipD)
+      this.tip.show(event, target)
     }
     this.props.onEnter(event, target)
   }
   onLeave (event) {
     let target = this.getDatum(event.target)
-    let tooltipD = {label: 'Hour-' + target.data.hour, counts: target.data.events.length}
+    let tooltipD = {}
     if (target && this.props.tipFunction) {
       this.tip.hide(event, tooltipD)
     }
@@ -128,42 +126,15 @@ class ForceDirectedGraph extends React.Component {
     } else if (target.children === null) {
       target.children = target._children
       target._children = null
-      this.reSet(this.props)
-      this.setSim(this.props)
-      this.simulation.alphaTarget(0.3).restart()
-    }
-    // else {
-    //   this.pos = [event.clientX, event.clientY]
-    //   this.isDrag = true
-    //   let pos = [this.nodes[i].x, this.nodes[i].y]
-    //   pos = pos.slice(0)
-    //   // console.log('FFG-oDStart-i', i)
-    //   this.nodes[i].fx = this.nodePos[0] = pos[0]
-    //   this.nodes[i].fy = this.nodePos[1] = pos[1]
-    //   this.simulation.alphaTarget(0.3).restart()
-    // }
-  }
-  onDrag (event) {
-    if (this.isDrag) {
-      let target = this.getDatum(event.target)
-      let tooltipD = {label: ' ', counts: target.data.events.length}
-      let i = this.getIndex(event.target)
-      this.nodes[i].fx = (event.clientX - this.pos[0]) + this.nodePos[0]
-      this.nodes[i].fy = (event.clientY - this.pos[1]) + this.nodePos[1]
-      // hiding tool tip on drag
-      if (target && this.props.tipFunction) {
-        this.tip.hide(event, tooltipD)
+      if (this.simOn) {
+        this.setTree(this.props)
+        this.setState({nodes: this.nodes, links: this.links})
+      } else {
+        this.reSet(this.props)
+        this.setSim(this.props)
+        this.simulation.alphaTarget(0.3).restart()
       }
     }
-  }
-  onDragEnd (event) {
-    this.isDrag = false
-    this.pos = [0, 0]
-    // console.log('FFG-oDE')
-    let i = this.getIndex(event.target)
-    this.nodes[i].fx = null
-    this.nodes[i].fy = null
-    this.simulation.alphaTarget(0)
   }
 
   updateDR (props) {
@@ -180,6 +151,7 @@ class ForceDirectedGraph extends React.Component {
 
   getDatum (target) {
     let i = target.getAttribute('data-index')
+    // console.log('FDGT-gD-i', i)
     return this.nodes[i]
   }
 
@@ -209,6 +181,9 @@ class ForceDirectedGraph extends React.Component {
     let tree = d3.tree().size([props.chartWidth, props.chartHeight])
     tree(this.rootNode)
     this.nodes = this.rootNode.descendants()
+    this.nodes.map((d, i) => {
+      d.key = i
+    })
     this.links = this.rootNode.links()
     // console.log('FDG-setTree')
   }
@@ -224,15 +199,13 @@ class ForceDirectedGraph extends React.Component {
     // console.log('FDG-draw-state', this.state)
     // console.log('FDG-draw-radius', props.radius)
     let events = {
-      // 'onMouseMove': this.onDrag,
       'onMouseDown': this.onDragStart,
-      // 'onMouseUp': this.onDragEnd,
       'onMouseEnter': this.onEnter,
       'onMouseLeave': this.onLeave
     }
     this.state.nodes.map((d, i) => {
       let circleProps = {
-        'data-key': d.data.index,
+        'data-key': d.key,
         'data-index': i,
         'r': props.radius,
         'cx': d.x,
@@ -241,8 +214,10 @@ class ForceDirectedGraph extends React.Component {
         'events': d.events,
         'hour': d.hour
       }
+      let key = d.key
+      if (key == null) { key = 'root' }
       nodeList.push(
-        <circle key={'cir-id' + d.data.index + '-hr-' + d.hour} {...events} {...circleProps} />
+        <circle key={'cir-id' + key + '-hr-' + d.hour} {...events} {...circleProps} />
       )
     })
     this.state.links.map((data, index) => {
@@ -286,15 +261,14 @@ class ForceDirectedGraph extends React.Component {
 }
 
 ForceDirectedGraph.defaultProps = {
-  xAccessor: 'x',
-  yAccessor: 'y',
   radius: 7,
   onClick: () => {},
   onEnter: () => {},
   onLeave: () => {},
   className: '',
   tipFunction: () => {},
-  isCurved: true
+  isCurved: true,
+  childAccessors: []
 }
 
 ForceDirectedGraph.propTypes = {
@@ -309,7 +283,8 @@ ForceDirectedGraph.propTypes = {
   onClick: PropTypes.func,
   onEnter: PropTypes.func,
   onLeave: PropTypes.func,
-  isCurved: PropTypes.bool
+  isCurved: PropTypes.bool,
+  childAccessors: PropTypes.array
 }
 
 export default ForceDirectedGraph
